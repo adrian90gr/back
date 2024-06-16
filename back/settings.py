6d31b01b9bc17bcdea136e5b95a82864f3005220
AdrianGonzalezRamirezEviden <adrian.gonzalezr.external@eviden.com>
2024-06-16 14:32:50 +0200

    commit final2

--- back/settings.py
+++ back/settings.py
@@ -53,16 +53,17 @@ MIDDLEWARE = [
 DATA_UPLOAD_MAX_MEMORY_SIZE = 10485760
 
 CORS_ALLOWED_ORIGINS = [
-    "http://127.0.0.1:5173",
-    "http://localhost:5173",
-    "https://chargeup-production.up.railway.app"
+    "https://chargeup-production.up.railway.app",
+    'https://unique-biscotti-ede739.netlify.app/'
+
 ]
 
 ALLOWED_HOSTS = [
     'back-b1i4.onrender.com',
     'localhost',
     '127.0.0.1',
-    'chargeup-production.up.railway.app'
+    'chargeup-production.up.railway.app',
+    'https://unique-biscotti-ede739.netlify.app/'
 ]
 
 CORS_ALLOW_CREDENTIALS = True
