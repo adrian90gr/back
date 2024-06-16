6264543acb137bb7a0883ad47a84f18a01af4100
AdrianGonzalezRamirezEviden <adrian.gonzalezr.external@eviden.com>
2024-06-16 11:35:23 +0200

    commit definitivo11

--- users/urls.py
+++ users/urls.py
@@ -3,10 +3,10 @@ from .views import CustomTokenObtainView, RegistroVista, ListaUsuariosVista, Det
 
 urlpatterns = [
     path('registro/', RegistroVista.as_view(), name='registro'),
-    path('users/', ListaUsuariosVista.as_view(), name='lista-usuarios'),
-    path('users/<int:pk>/', DetalleUsuarioVista.as_view(), name='detalle-usuario'),
-    path('users/<int:pk>/actualizar/', ActualizarUsuarioVista.as_view(), name='actualizar-usuario'),
-    path('users/<int:pk>/eliminar/', EliminarUsuarioVista.as_view(), name='eliminar-usuario'),
+    path('usuarios/', ListaUsuariosVista.as_view(), name='lista-usuarios'),
+    path('usuarios/<int:pk>/', DetalleUsuarioVista.as_view(), name='detalle-usuario'),
+    path('usuarios/<int:pk>/actualizar/', ActualizarUsuarioVista.as_view(), name='actualizar-usuario'),
+    path('usuarios/<int:pk>/eliminar/', EliminarUsuarioVista.as_view(), name='eliminar-usuario'),
     path('login/', CustomTokenObtainView.as_view(), name='token_obtain_pair'),
     path('me/', UsuarioActualVista.as_view(), name='usuario_actual'),  
 ]
